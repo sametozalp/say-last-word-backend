cfce5709c6365ed89ac73d2871393ede4fa5842d
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-12-28 00:21:05 +0300

    Initial commit

---
+++ auth-service/src/test/java/com/ozalp/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozalp.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
