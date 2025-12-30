36cf7a3e62a1f05de1576989a38c7abdd699cb85
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-12-30 22:38:24 +0300

    add cors all services

--- api-gateway/src/main/java/com/ozalp/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ozalp/api_gateway/ApiGatewayApplication.java
@@ -2,14 +2,30 @@ package com.ozalp.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    // cors problem solved
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*"); // Tüm metodlara izin ver (GET, POST vs.)
+            }
+        };
+    }
 
 }
--- user-profile-service/src/main/java/com/ozalp/user_profile_service/UserProfileServiceApplication.java
+++ user-profile-service/src/main/java/com/ozalp/user_profile_service/UserProfileServiceApplication.java
@@ -2,12 +2,29 @@ package com.ozalp.user_profile_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserProfileServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserProfileServiceApplication.class, args);
 	}
 
+	// cors problem solved
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("*"); // Tüm metodlara izin ver (GET, POST vs.)
+			}
+		};
+	}
 }
