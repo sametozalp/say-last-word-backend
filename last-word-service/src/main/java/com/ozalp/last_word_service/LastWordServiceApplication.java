22f79a9f8f2a124b56034ba557ce16e7825d7e57
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-12-30 21:50:31 +0300

    fix cors problem

--- last-word-service/src/main/java/com/ozalp/last_word_service/LastWordServiceApplication.java
+++ last-word-service/src/main/java/com/ozalp/last_word_service/LastWordServiceApplication.java
@@ -2,12 +2,30 @@ package com.ozalp.last_word_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
 public class LastWordServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LastWordServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LastWordServiceApplication.class, args);
+    }
+
+    // cors problem solved
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*"); // Tüm metodlara izin ver (GET, POST vs.)
+            }
+        };
+    }
 
 }
